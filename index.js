71a2395cabf351fc6c5f02097992d3e80b1cebc5
mehedi hasan <mhshohan04@gmail.com>
2025-11-05 15:12:24 +0600

    server connected to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -10,6 +11,48 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Server is running!')
 })
+
+//
+
+
+const uri = "mongodb+srv://smartDB:BcPy1TkSmRbVH5DK@cluster0.jwgya6a.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('smart_db');
+    const productsCollection = db.collection('products');
+
+
+    app.post('/products', async(req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
